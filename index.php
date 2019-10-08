d34ecb98630bcb07f61b049abd9d5496b7f23a24
RobinMarien <dev.robinmarien@gmail.com>
2019-10-08 12:31:22 +0200

    working alerts (for empty fields)

--- index.php
+++ index.php
@@ -39,18 +39,57 @@ if(isset($_GET["food"]) && $_GET["food"] == 0){
 $totalValue = 0;
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $alerts = [];
+    $errors = [];
+
     //E-MAIL
     $email = test_input($_POST["email"]);
-    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        echo("<div class='alert alert-danger' role='alert'><a href='#email' class='alert-link'>$email</a> is not a valid email addres!</div>");
+    //if empty, create alert
+    if (empty($email)) {
+        $alerts[] = "Please fill in your <a href='#email' class='alert-link'>E-mail</a>!";
     }
+    else {
+        //if invalid email, create error
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            //echo("<div class='alert alert-danger' role='alert'><a href='#email' class='alert-link'>$email</a> is not a valid email addres!</div>");
+            $errors[] = "<a href='#email' class='alert-link'>$email</a> is not a valid email addres!";
+        }
+    }
+
+
     //ADDRESS
     $street = test_input($_POST["street"]);
     $streetnumber = test_input($_POST["streetnumber"]);
     $city = test_input($_POST["city"]);
     $zipcode = test_input($_POST["zipcode"]);
 
-    
+    if (empty($street)){
+        $alerts[] = "Please fill in your <a href='#street' class='alert-link'>street</a>!";
+    }
+
+    if (empty($streetnumber)){
+        $alerts[] = "Please fill in your <a href='#streetnumber' class='alert-link'>street number</a>!";
+    }
+    else{
+        if (!is_numeric($streetnumber)){
+            $errors[] = "<a href='#streetnumber' class='alert-link'>Street Number</a> only accepts numbers!";
+        }
+    }
+
+    if (empty($city)){
+        $alerts[] = "Please fill in your <a href='#city' class='alert-link'>city</a>!";
+    }
+
+    if (empty($zipcode)){
+        $alerts[] = "Please fill in your <a href='#zipcode' class='alert-link'>zipcode</a>!";
+    }
+    else{
+        if (!is_numeric($zipcode)){
+            $errors[] = "<a href ='#zipcode' class='alert-link'>Zipcode</a> only accepts numbers!";
+        }
+    }
+
 
 }
 
@@ -60,4 +99,20 @@ function test_input($data) {
     $data = htmlspecialchars($data); // Convert special characters to HTML entities ( < = &lt; , ... )
     return $data;
 }
+
+// DISPLAY ERRORS
+if (!empty($errors)){
+    foreach ($errors as $error){
+        echo ("<div class='alert alert-danger' role='alert'>" . $error . "</div>");
+    }
+}
+
+// DISPLAY ALERTS
+if (!empty($alerts)){
+    foreach ($alerts as $alert){
+        echo ("<div class='alert alert-warning' role='alert'>" . $alert . "</div>");
+    }
+}
+
+
 require 'form-view.php';
